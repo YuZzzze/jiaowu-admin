d5a66f647080be8a5e74bd1b0710ee762ab4888a
YuZzzze <92770228+YuZzzze@users.noreply.github.com>
2023-09-07 08:10:04 +0800

    feat: PageContainer

---
+++ src/components/PageContainer/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="bg-white p-4 rounded shadow-md h-full">
+    <!-- 页面标题 -->
+    <h2 class="text-2xl font-semibold mb-4">{{ pageTitle }}</h2>
+
+    <!-- 操作按钮 -->
+    <div class="flex justify-between items-center mb-4">
+      <slot name="actions"></slot>
+    </div>
+
+    <!-- 主要内容 -->
+    <div>
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref, defineProps } from 'vue';
+
+const props = defineProps({
+  pageTitle: String
+});
+</script>
+
+<style scoped>
+/* 在这里可以添加自定义样式 */
+</style>
