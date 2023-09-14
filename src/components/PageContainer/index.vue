c090fb89d881efe0b2da5454e875591416a459e6
YuZzzze <92770228+YuZzzze@users.noreply.github.com>
2023-09-14 23:53:55 +0800

    fix: 修改布局

--- src/components/PageContainer/index.vue
+++ src/components/PageContainer/index.vue
@@ -1,16 +1,17 @@
 <template>
-  <div class="bg-white p-4 rounded shadow-md h-full">
+  <div class="h-full flex flex-col flex-1">
     <!-- 页面标题 -->
-    <h2 class="text-2xl font-semibold mb-4">{{ pageTitle }}</h2>
+    <h1 class="text-2xl font-semibold mb-4">{{ pageTitle }}</h1>
+    <div class="bg-white p-4 rounded shadow-md flex-grow">
+      <!-- 操作按钮 -->
+      <div class="flex justify-between items-center mb-4">
+        <slot name="actions"></slot>
+      </div>
 
-    <!-- 操作按钮 -->
-    <div class="flex justify-between items-center mb-4">
-      <slot name="actions"></slot>
-    </div>
-
-    <!-- 主要内容 -->
-    <div>
-      <slot></slot>
+      <!-- 主要内容 -->
+      <div>
+        <slot></slot>
+      </div>
     </div>
   </div>
 </template>
@@ -22,7 +23,3 @@ const props = defineProps({
   pageTitle: String
 });
 </script>
-
-<style scoped>
-/* 在这里可以添加自定义样式 */
-</style>
